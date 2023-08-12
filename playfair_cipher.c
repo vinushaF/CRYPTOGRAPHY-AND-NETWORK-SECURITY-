5cd5911a313f09ba11082564f45afdd7d8d2bea0
vinushaF <112194872+vinushaF@users.noreply.github.com>
2023-08-12 08:38:59 +0530

    implement playfair cipher

---
+++ playfair_cipher.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#define MX 5
+void playfair(char ch1,char ch2, char key[MX][MX])
+{
+int i,j,w,x,y,z;
+FILE *out;
+if((out=fopen("cipher.txt","a+"))==NULL)
+{
+printf("File Currupted.");
+}
+for(i=0;i<MX;i++)
+{
+}}}
+for(j=0;j<MX;j++)
+{
+if(ch1==key[i][j])
+{
+w=i;
+x=j;
+}
+else if(ch2==key[i][j])
+{
+y=i;
+z=j;
+//printf("%d%d %d%d",w,x,y,z);
+if(w==y)
+{
+x=(x+1)%5;z=(z+1)%5;
+printf("%c%c",key[w][x],key[y][z]);
+fprintf(out, "%c%c",key[w][x],key[y][z]);
+}
+else if(x==z)
+{
+w=(w+1)%5;y=(y+1)%5;
+printf("%c%c",key[w][x],key[y][z]);
+fprintf(out, "%c%c",key[w][x],key[y][z]);
+k=0;
+for(i=0;i<MX;i++)
+{
+for(j=0;j<MX;j++)
+{
+if(k<n)
+{
+key[i][j]=keystr[k];
+k++;}
+else
+{
+key[i][j]=keyminus[m];m++;
+}
+printf("%c ",key[i][j]);
+}
+printf("\n");
+}
+printf("\n\nEntered text :%s\nCipher Text :",str);
+for(i=0;i<strlen(str);i++)
+{
+if(str[i]=='J')str[i]='I';
+if(str[i+1]=='\0')
+playfair(str[i],'X',key);
+else
+{
+}
+getch();
+}
+if(str[i+1]=='J')str[i+1]='I';
+if(str[i]==str[i+1])
+playfair(str[i],'X',key);
+else
+{
+playfair(str[i],str[i+1],key);i++;
